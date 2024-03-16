4946d2abcd2f5157fc8716d5340c0107e7771fb8
robcranfill <robcranfill@gmail.com>
2024-03-16 09:02:50 -0700

    handle gestures

--- RadioPlayer.py
+++ RadioPlayer.py
@@ -6,25 +6,27 @@ import time
 
 class RadioPlayer:
 
+    """Station list is list of (station name, station URL)"""
     def __init__(self, station_list):
 
         self._station_list = station_list
         self._selected_station_index = 0
 
         self._client = MPDClient()
+        
         self._client.timeout = 10                # network timeout in seconds (floats allowed), default: None
         self._client.idletimeout = None          # timeout for fetching the result of the idle command, default: None
         self._client.connect("localhost", 6600)
 
         for s in station_list:
-            self._client.add(s)
+            self._client.add(s[1])
 
 
     def __del__(self):
+        self._client.stop()
         self._client.close()
         self._client.disconnect()
         print("RadioPlayer destroyed.")
-
 
     def start_playing(self):
         print("RadioPlayer starting.")
@@ -34,38 +36,35 @@ class RadioPlayer:
         print("RadioPlayer stopping.")
         self._client.stop()
 
-
     def next_station(self):
         '''Start playing the next station in the station list.'''
         self._selected_station_index = self._selected_station_index + 1
         if self._selected_station_index == len(self._station_list):
             self._selected_station_index = 0
-        print(f"Station is now {self._selected_station_index}")
+        print(f"Station is now {self._station_list[self._selected_station_index][0]}")
 
     def prev_station(self):
         '''Start playing the previous station in the station list.'''
         self._selected_station_index = self._selected_station_index - 1
         if self._selected_station_index < 0:
             self._selected_station_index = len(self._station_list) - 1
-        print(f"Station is now {self._selected_station_index}")
-
+        print(f"Station is now {self._station_list[self._selected_station_index][0]}")
 
     def volume_up(self):
-        print("Volume up!")
+        print("Volume up?")
 
     def volume_down(self):
-        print("Volume down!")
+        print("Volume down?")
 
     
 # for testing
 if __name__ == "__main__":
 
     rp = RadioPlayer(["http://uk1.internet-radio.com:8355/stream",
-                                  "http://relay2.slayradio.org:8000"])
+                      "http://relay2.slayradio.org:8000"])
 
     rp.start_playing()
     time.sleep(10)
     rp.stop_playing()
 
-# done
 
--- piradio.py
+++ piradio.py
@@ -1,34 +1,53 @@
 # piradio, by cran
 
 import OLEDDisplay
+from RadioPlayer import RadioPlayer
 
-# from mpd import MPDClient
+import board
+import busio
+import adafruit_apds9960.apds9960
+
 import time
 
-
-# client = MPDClient()
-# client.timeout = 10                # network timeout in seconds (floats allowed), default: None
-# client.idletimeout = None          # timeout for fetching the result of the idle command, default: None
-# client.connect("localhost", 6600)
-
-
-disp = OLEDDisplay.OLEDDisplay(2)
-disp.draw_messages(["Hello, cran!", "What's up?"])
-
-print(f"mpd client version {client.mpd_version}")
-
-url = "http://uk1.internet-radio.com:8355/stream"
-
-client.add(url)
-
-print(f"playing {url}....")
-client.play()
-
-time.sleep(10)
-client.stop()
-print("stopped.")
+GESTURE_UP    = 1
+GESTURE_DOWN  = 2
+GESTURE_LEFT  = 3
+GESTURE_RIGHT = 4
 
 
-client.close()                     # send the close command
-client.disconnect()                # disconnect from the server
+if __name__ == "__main__":
 
+    urls = [
+        ("Radio Dublin", "http://uk1.internet-radio.com:8355/stream"),
+        ("Radio Gaga",   "http://relay2.slayradio.org:8000")
+        ]
+
+
+    disp = OLEDDisplay.OLEDDisplay(2)
+    disp.draw_messages(["PiRadio v0.1", urls[0][0]])
+
+    i2c = busio.I2C(board.SCL, board.SDA)
+    sensor = adafruit_apds9960.apds9960.APDS9960(i2c)
+    sensor.enable_proximity = True
+    sensor.enable_gesture = True
+
+
+    rp = RadioPlayer(urls)
+    rp.start_playing()
+
+    while True:
+        gesture = sensor.gesture()
+        if gesture != 0:
+        #     print('Saw gesture: {0}'.format(gesture))
+            if gesture == GESTURE_LEFT:
+                rp.prev_station()
+                # print("Prev!")
+            elif gesture == GESTURE_RIGHT:
+                rp.next_station()
+                # print("Next!")
+            elif gesture == GESTURE_UP:
+                print("UP?!")
+            elif gesture == GESTURE_DOWN:
+                print("DOWN?!")
+            else:
+                print("What?!")
